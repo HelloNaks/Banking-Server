99684a6532b5bcb39088ce434f4acfacdf95496e
HelloNaks <krh963852@daum.net>
2023-02-19 19:22:51 +0900

    docs : 프로젝트 세팅

---
+++ src/main/java/com/numble/bankingServer/BankingServerApplication.java
@@ -0,0 +1,13 @@
+package com.numble.bankingServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingServerApplication.class, args);
+	}
+
+}
